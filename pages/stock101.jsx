918c1b4b1f8941e1856cd56d1103669e120fe62d
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2023-03-11 00:59:25 -0500

    refactor stock101 code

--- pages/stock101.jsx
+++ pages/stock101.jsx
@@ -46,13 +46,7 @@ const stock101 = () => {
           >
             <button className="px-8 py-2 mt-4 mr-8">Code</button>
           </a>
-          {/* <a
-            href="https://gear-ecommerce-app-juntaodev.vercel.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <button className="px-8 py-2 mt-4">Demo</button>
-          </a> */}
+          
         </div>
         <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
           <div className="p-2">
